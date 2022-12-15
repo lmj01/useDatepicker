f44f2a947523dd98adc63f3305e52b1c6dbe938a
lmj01 <lmj01@users.noreply.github.com>
2022-12-15 11:50:27 +0800

    update vue3

--- PickDate.vue
+++ PickDate.vue
@@ -1,60 +1,53 @@
 <template>
     <input ref="refElInput" type="text" class="form-control date" :class="size" required>
 </template>
-<script>
-import { defineComponent, reactive, onMounted, ref } from 'vue';
+<script setup>
+import { reactive, onMounted, ref, defineProps, defineEmits, defineExpose } from 'vue';
 import Datepicker from './js/Datepicker';
 import zhCN from './js/i18n/locales/zh-CN';
-export default defineComponent({
-    name: 'PickDatev5',
-    emits: ['value'], 
-    props: {
-        size: {
-            type: String,
-            default: '',
-        },
-    },    
-    setup(props, ctx) {
-        const refElInput = ref(null);
-        const ud = reactive({
-            datepicker: null,
-            bk: '',
-        });
-        Object.assign(Datepicker.locales, zhCN);
-        function initial() {        
-            ud.datepicker = new Datepicker(refElInput.value, {
-                format: 'yyyy-mm-dd',
-                language: 'zh-CN',
-                buttonClass: 'btn',
-            });
-            refElInput.value.addEventListener('changeDate', function(event){
-                ctx.emit('value', event.target.value);
-            });
-        }
-        function update(value) {
-            refElInput.value.value = value;
-        }
-        function lockDate(isLock) {
-            ud.bk = refElInput.value.value;
-            let newOption = {
-                maxDate: null,
-                minDate: null,
-            }
-            if (isLock) {
-                let today = new Date();            
-                newOption.maxDate = `${today.getFullYear()}-${today.getMonth()+1}-${today.getDate()}`;
-                newOption.minDate = `${today.getFullYear()-100}-${today.getMonth()+1}-${today.getDate()}`;
-            }
-            ud.datepicker.setOptions(newOption);
-            refElInput.value.value = ud.bk;
-        }
-        onMounted(() => initial());
-        return {
-            ud,
-            refElInput,
-            update,
-            lockDate,
-        };
+const props = defineProps({
+    size: {
+        type: String,
+        default: '',
     },
 });
+const emit = defineEmits(['value']);
+const refElInput = ref(null);
+const ud = reactive({
+    datepicker: null,
+    bk: '',
+});
+Object.assign(Datepicker.locales, zhCN);
+function initial() {        
+    ud.datepicker = new Datepicker(refElInput.value, {
+        format: 'yyyy-mm-dd',
+        language: 'zh-CN',
+        buttonClass: 'btn',
+    });
+    refElInput.value.addEventListener('changeDate', function(event){
+        ctx.emit('value', event.target.value);
+    });
+}
+function update(value) {
+    refElInput.value.value = value;
+}
+function lockDate(isLock) {
+    ud.bk = refElInput.value.value;
+    let newOption = {
+        maxDate: null,
+        minDate: null,
+    }
+    if (isLock) {
+        let today = new Date();            
+        newOption.maxDate = `${today.getFullYear()}-${today.getMonth()+1}-${today.getDate()}`;
+        newOption.minDate = `${today.getFullYear()-100}-${today.getMonth()+1}-${today.getDate()}`;
+    }
+    ud.datepicker.setOptions(newOption);
+    refElInput.value.value = ud.bk;
+}
+onMounted(() => initial());
+defineExpose({
+    update,
+    lockDate,
+})
 </script>
--- PickDateRange.vue
+++ PickDateRange.vue
@@ -1,5 +1,5 @@
 <template>
-    <div ref="refElPickRange" id="idPickDateRange" class="position-relative row">
+    <div ref="refElPickRange" class="position-relative row">
         <div class="col-auto p-0 m-0">
             <a class="btn" disabled v-html="labelFrom"></a>
         </div>
@@ -14,52 +14,44 @@
         </div>
     </div>
 </template>
-<script>
-import { defineComponent, reactive, onMounted, ref } from 'vue'
+<script setup>
+import { reactive, onMounted, ref } from 'vue'
 import Datepicker from './js/Datepicker'
 import DateRangePicker from './js/DateRangePicker'
 import zhCN from './js/i18n/locales/zh-CN'
-export default defineComponent({
-    name: 'PickDateRangev5',
-    props: {
-        labelFrom: {
-            type: String,
-            default: '',
-        },
-        labelTo: {
-            type: String,
-            default: '',
-        },
-        size: {
-            type: String,
-            default: '',
-        },
+const props = defineProps({
+    labelFrom: {
+        type: String,
+        default: '',
     },
-    emits: ['update'],
-    setup(props, ctx) {
-        const refElPickRange = ref(null);
-        const ud = reactive({
-            idEl: 'idPickDateRange',
-            datepicker: null,
+    labelTo: {
+        type: String,
+        default: '',
+    },
+    size: {
+        type: String,
+        default: '',
+    },
+});
+const emit = defineEmits(['update']);
+const refElPickRange = ref(null);
+const datepicker = ref(null);
+Object.assign(Datepicker.locales, zhCN);
+const initial = () => {      
+    datepicker.value = new DateRangePicker(refElPickRange.value, {
+        format: 'yyyy-mm-dd',
+        language: 'zh-CN',
+        buttonClass: 'btn',
+    });
+    refElPickRange.value.querySelectorAll('input').forEach((input) => {
+        input.addEventListener('changeDate', (event) => {
+            const target = event.target;
+            emit('update', target.getAttribute('name'), target.value);
         });
-        Object.assign(Datepicker.locales, zhCN);
-        const initial = () => {                
-            ud.datepicker = new DateRangePicker(document.getElementById(ud.idEl), {
-                format: 'yyyy-mm-dd',
-                language: 'zh-CN',
-                buttonClass: 'btn',
-            });
-            document.querySelectorAll(`#${ud.idEl} input`).forEach((input) => {
-                input.addEventListener('changeDate', (event) => {
-                    const target = event.target;
-                    ctx.emit('update', target.getAttribute('name'), target.value);
-                });
-            });
-        }
-        onMounted(() => initial());
-        return {
-            ud,
-        };
-    },
+    });
+}
+onMounted(() => initial());
+defineExpose({
+
 });
 </script>
