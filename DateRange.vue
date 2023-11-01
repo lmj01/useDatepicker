57ac8573bc0120285ddf75cba13c5ed1acb932eb
lmj01 <lmj01@users.noreply.github.com>
2023-11-01 16:21:15 +0800

    add

---
+++ DateRange.vue
@@ -0,0 +1,63 @@
+<template>
+    <div ref="elPickRange" class="position-relative row">
+        <div class="col-auto p-0 m-0">
+            <input type="text" name="range-start" class="form-control text-center" :placeholder="labelFrom" :class="inputStyle" :required="must">
+        </div>
+        <div class="col-auto p-0 m-0">
+            <input type="text" name="range-end" class="form-control text-center" :placeholder="labelTo" :class="inputStyle" :required="must">
+        </div>
+    </div>
+</template>
+<script setup>
+import { reactive, onMounted, ref, computed } from 'vue'
+import Datepicker from './js/Datepicker'
+import DateRangePicker from './js/DateRangePicker'
+import zhCN from './js/i18n/locales/zh-CN'
+const props = defineProps({
+    labelFrom: {
+        type: String,
+        default: '',
+    },
+    labelTo: {
+        type: String,
+        default: '',
+    },
+    size: {
+        type: String,
+        default: '',
+    },
+    must: {
+        type: Boolean,
+        default: true,
+    },
+});
+const emit = defineEmits(['update']);
+const elPickRange = ref(null);
+const datepicker = ref(null);
+const btnStyle = computed(()=>{
+    if (props.size) return 'btn-'+props.size;
+    return '';
+})
+const inputStyle = computed(()=>{
+    if (props.size) return 'form-control-'+props.size;
+    return '';
+})
+Object.assign(Datepicker.locales, zhCN);
+const initial = () => {      
+    datepicker.value = new DateRangePicker(elPickRange.value, {
+        format: 'yyyy-mm-dd',
+        language: 'zh-CN',
+        buttonClass: 'btn',
+    });
+    elPickRange.value.querySelectorAll('input').forEach((input) => {
+        input.addEventListener('changeDate', (event) => {
+            const target = event.target;
+            emit('update', target.getAttribute('name'), target.value);
+        });
+    });
+}
+onMounted(() => initial());
+defineExpose({
+
+});
+</script>
